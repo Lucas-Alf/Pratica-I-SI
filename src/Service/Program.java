a58abfefe44314547cd53a0397cfb2b099e7b726
Lucas Machado Alf <42899268+Lucas-Alf@users.noreply.github.com>
2018-09-12 11:35:28 -0300

    Update Program.java

--- src/Service/Program.java
+++ src/Service/Program.java
@@ -5,6 +5,7 @@ import View.GridCarro;
 public class Program {
 
     public static void main(String[] args) {
+        //java.util.logging.Logger.getLogger("org.hibernate").setLevel(Level.SEVERE);
        new GridCarro().setVisible(true);
     }
 }
